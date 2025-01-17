24923f7f021c76323d8cad79736b73c11a740416
Benni <miuta.beni@gmail.com>
2025-01-17 16:51:12 +0100

    Implemented logical and mathematical operation pages
    Implemented logical and mathematical operation pages

---
+++ vue-mpa/src/components/LogicOperations.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Logical Operations</h1>
+      <p>Feel free to reach out through this page.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "LogicalOperations",
+  };
+  </script>
+  
